60e8ba78ba46a172ef8a8fa119ca34f6c444844e
MinhHT <minhbeo135@gmail.com>
2020-01-29 13:57:09 +0700

    update menu compoent

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import React, {
+    Component
+} from 'react';
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+
+    }
+}
+export default Menu;
