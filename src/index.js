737bdd028d9ebef5d82e68b306cc08ae7fb38c04
MinhHT <minhbeo135@gmail.com>
2020-02-07 16:32:19 +0700

    add awesome font and bootstrap-social package to main project(index.js)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/assets/css/bootstrap-social.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
